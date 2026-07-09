1fbe04f7ebcebef1b3a86420246f25f23322eced
RafaelRaposoDeQuadros <rafaelraposodequadros@gmail.com>
2026-07-09 01:15:07 -0300

    feat: configure backend infrastructure

---
+++ backend/src/main/java/com/flux/backend/shared/health/HealthController.java
@@ -0,0 +1,15 @@
+package com.flux.backend.shared.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping
+    public String health(){
+        return "Flux backend Online";
+    }
+}
